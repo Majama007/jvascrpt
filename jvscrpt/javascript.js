6257242e1840f5f13ebe8f661345b1e8ba385ebe
Majama007 <105103519+Majama007@users.noreply.github.com>
2022-06-18 23:54:10 -0700

    Add files via upload

---
+++ jvscrpt/javascript.js
@@ -0,0 +1,9 @@
+
+function changeImage() {
+    ;
+    if (document.getElementById("imgClickAndChange").src == "wp8422895.jpg"){
+        document.getElementById("imgClickAndChange").src = "HD.jpg";
+    } else {
+        document.getElementById("imgClickAndChange").src = "HD.jpg";
+    }
+}
